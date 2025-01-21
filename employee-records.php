54215a485af2b4885aba76e02a88c21a3cff2874
Azuka171 <67867738+Azuka171@users.noreply.github.com>
2025-01-21 05:42:25 +0100

    tittle page
    we added tittle pages

--- employee-records.php
+++ employee-records.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Employee Records Page </title>
 </head>
 <body>
     <h2>Employee</h2>
--- staff-onboarding.php
+++ staff-onboarding.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Employee onboarding page</title>
 </head>
 <body>
     <?php if(isset($_SESSION['sucess'])) { ?>
